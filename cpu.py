dd3a45cf23aa01fdfcb8c43d395c6d8ab9bf94de
Shadowborn <james.starks023@gmail.com>
2020-02-28 09:59:59 -0700

    workin on load and alu

--- cpu.py
+++ cpu.py
@@ -13,6 +13,7 @@ class myCPU:
         self.ram = [0] * 256
         # Create stack pointer
         self.SP = 7
+        self.im = 5
     #INITALIZE OPERATION CODES
         self.op_codes = {
             'PUSH': 0b01000101,
@@ -30,4 +31,91 @@ class myCPU:
             'RET': 0b00010001,
         }
         #set flag to my ls-8
-        self.flag = 0b0000000 
+        self.flag = 0b0000000
+
+    def load(self, file):
+        address = 0
+
+        program = []
+
+        if file is None:
+            print("ERROR: The file is not working")
+            sys.exit(1)
+
+        try:
+            with open(file, 'r') as f:
+                for line in f:
+                    split_comment = line.split('#')
+                    number = split_comment[0]
+
+                    try:
+                        x = int(number, 2)
+                    except ValueError:
+                        continue
+                    print(f"{x:08b}: {x:d}")
+                    program.append(x)
+        except ValueError:
+            print(f"File not found")
+
+        for instructions in program:
+            self.ram[address] = instructions
+            address += 1
+
+    def jmp(self, address, *args):
+        self.pc = self.register[address]
+
+    def jeq(self, address, *args):
+        mask = 0b00000001
+        if mask & self.im == 1:
+            self.pc = self.register[address]
+        else:
+            self.pc += 2
+
+    def jne(self, address, *args):
+        mask = 0b00000001
+        if mask & self.im == 0:
+            self.pc = self.register[address]
+        else:
+            self.pc += 2
+
+    def alu(self, op, reg_a, reg_b):
+
+        # mask == 0b00000001
+
+        # if op == self.op_codes['JMP']:
+        #     self.pc = self.register[address]
+
+        # if op == self.op_codes['JEQ']:
+        #     if mask & self.im == 1:
+        #         self.pc = self.register[address]
+        #     else:
+        #         self.pc += 2
+
+        # if op == self.op_codes['JNE']:
+        #     if mask & self.im == 0:
+        #         self.pc = self.register[address]
+        #     else:
+        #         self.pc += 2
+
+        if op == self.op_codes['ADD']:
+            self.register[reg_a] += self.register[reg_b]
+            self.pc += 3
+        
+        elif op == self.op_codes['MUL']:
+            self.register[reg_a] *= self.register[reg_b]
+            self.pc += 3
+
+        elif op == self.op_codes['CMP']:
+            
+            if reg_a == reg_b:  # if a == b, set E to 1
+                self.flag = 0b00000001
+
+            elif reg_a < reg_b:  # if a < b, set L to 1
+                self.flag = 0b00000100
+
+            elif reg_a > reg_b:  # if a > b, set G to 1
+                self.flag = 0b00000010
+
+        else:
+            raise Exception("Unsupported ALU operation")
+        
