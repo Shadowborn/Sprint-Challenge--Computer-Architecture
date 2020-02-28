48290062b0c0a69e271298fe91384b388073237d
Shadowborn <james.starks023@gmail.com>
2020-02-28 09:32:44 -0700

    initialiezed operation codes and set flag to ls-8

--- cpu.py
+++ cpu.py
@@ -13,4 +13,21 @@ class myCPU:
         self.ram = [0] * 256
         # Create stack pointer
         self.SP = 7
-        
+    #INITALIZE OPERATION CODES
+        self.op_codes = {
+            'PUSH': 0b01000101,
+            'POP': 0b01000110,
+            'ADD': 0b10100000,
+            'JEQ': 0b01010101,
+            'JNE': 0b01010110,
+            'JMP': 0b01010100,
+            'CMP': 0b10100111,
+            'LDI': 0b10000010,
+            'PRN': 0b01000111,
+            'HLT': 0b00000001,
+            'MUL': 0b10100010,
+            'CALL': 0b01010000,
+            'RET': 0b00010001,
+        }
+        #set flag to my ls-8
+        self.flag = 0b0000000 
